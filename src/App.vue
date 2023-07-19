f0108d2e04474663745fb71ecf43599b253e18b3
Crinax <agreedeth@gmail.com>
2023-07-19 06:41:57 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container">
+    <div class="contaienr__content">
+      Nothing to see here...
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-items: center;
+  align-items: center;
+}
+
+.container__content {
+  display: flex;
+  flex-direction: column;
+}
+</style>
