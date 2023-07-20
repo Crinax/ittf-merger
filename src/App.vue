0c227eeedd99eb73baaa9363d1a02a089e25bc94
Crinax <agreedeth@gmail.com>
2023-07-20 14:24:22 +0300

    feat(ui): add file input

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <script setup lang="ts">
+import AppFileLoader from './components/AppFileLoader.vue'
+
+import { ref, watch } from 'vue';
+
+const errorMessage = ref('');
+const file = ref<File | undefined>(undefined);
+
+watch(file, (newFile, oldFile) => {
+  if (newFile === undefined) {
+    return;
+  }
+
+  if (newFile?.type.startsWith('image')) {
+    errorMessage.value = 'Невозможно загрузить такой тип файла';
+    file.value = oldFile;
+  }
+
+  errorMessage.value = '';
+})
 </script>
 
 <template>
   <div class="container">
-    <div class="contaienr__content">
-      Nothing to see here...
-    </div>
+    <p v-if="errorMessage.length">
+      {{ errorMessage }}
+    </p>
+    <app-file-loader v-model="file" />
   </div>
 </template>
 
 <style scoped>
 .container {
-  display: flex;
-  justify-items: center;
-  align-items: center;
-}
-
-.container__content {
   display: flex;
   flex-direction: column;
 }
---
+++ src/components/AppFileLoader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+export interface AppFileLoaderProps {
+  modelValue?: File,
+}
+
+export interface AppFileLoaderEmits {
+  (e: 'update:modelValue', value: File): void,
+}
+
+defineProps<AppFileLoaderProps>();
+const emit = defineEmits<AppFileLoaderEmits>();
+
+const computeFilePath = (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  const [file] = target.files;
+
+  emit('update:modelValue', file);
+}
+</script>
+
+<template>
+  <input class="hidden-input" type="file" @change="computeFilePath" />
+</template>
